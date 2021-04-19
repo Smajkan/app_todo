1f1d40e51b104ec4c6fddd46c11d23f256e60a64
Smajkan <aldinsmajkan2@gmail.com>
2021-04-19 16:39:19 +0200

    FirstChanges

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header>Aldin's ToDo list</header>
+
 
     </div>
   );
