32871558b6e69c3e352fa496fb85386c4aab8500
Smajkan <aldinsmajkan2@gmail.com>
2021-04-19 17:05:38 +0200

    Dodan inputTextHandler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css';
 //Ubacivanje komponenata
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+
+
   return (
     <div className="App">
       <header>Aldin's ToDo list</header>
 
       <Form></Form>
       <ToDoList></ToDoList>
+
+
+
     </div>
   );
 }
