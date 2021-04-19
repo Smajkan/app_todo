37001b367a0b0ed4ed68369bc14006e4c7b16d4c
Smajkan <aldinsmajkan2@gmail.com>
2021-04-19 16:52:59 +0200

    Dodane ikone i fontovi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import './App.css';
+//Ubacivanje komponenata
+import Form from './components/Form';
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <header>Aldin's ToDo list</header>
 
 
+      <Form></Form>
     </div>
   );
 }
