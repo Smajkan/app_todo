1e083f5fdf0a11c733e9e470abd67ff908b4e4da
Smajkan <aldinsmajkan2@gmail.com>
2021-04-19 19:26:38 +0200

    Error inside of deleteHandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,17 @@ import React from 'react'
 const Todo = ({ text, todo, todos, setTodos }) => {
     //Eventi
     const deleteHandler = () => {
-        setTodos(todos.filter((el) => el.id !== todo.id))
-
+        setTodos(todos.filter((el) => el.id !== todo.id));
     };
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, oompleted: !item.completed
+                }
+            }
+        })
+    }
 
 
     return (
