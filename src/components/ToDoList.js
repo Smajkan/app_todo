a23d319363bfe591913bd4b6d00d0c0bf58dcea4
Smajkan <aldinsmajkan2@gmail.com>
2021-04-19 17:45:47 +0200

    Dodan Todo u ToDoList i dodani buttoni

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 
-//
+//Ubacivanje Todo
+import Todo from './Todo'
 const ToDoList = () => {
     return (
         <div className="todo-container">
             <ul className="todo-list"></ul>
+            <Todo />
         </div>
 
     );
